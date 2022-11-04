58aabd4b65277ec8e0eb2bea846ad27221c05467
Rakesh <byreddyrakesh41@gmail.com>
2022-11-04 19:48:07 +0530

    Added: Code for the Maximum Between three Float Numbers

---
+++ src/generics/MaximumBetweenThreeFloats.java
@@ -0,0 +1,59 @@
+package generics;
+//Use Case 1 is to give 3 integers and find out the maximum and its position in array.
+//Use Case 2 is to give 3 floats and find out the maximum and its position in array.
+import java.util.Scanner;
+public class MaximumBetweenThreeFloats {
+
+        //Default Constructor
+        public MaximumBetweenThreeFloats()
+        {
+            System.out.println("Welcome to Generic Assignment Problem.");
+        }
+
+        public static void main(String[] args) {
+            //Initialization of object.
+            MaximumBetweenThreeFloats testMaximum = new MaximumBetweenThreeFloats();
+
+            //Define the array where size is 3(given).
+            Float[] array = addToArray();
+            printArray(array);
+            getMaximum(array);
+        }
+
+        //Get maximum from the array and its position.
+        public static void getMaximum(Float[] array) {
+            float max = array[0];
+            int position = 0;
+            for (int i = 0; i < array.length; i++)
+            {
+                int a = array[i].compareTo(max);
+                if(a > 0)
+                {
+                    max = array[i];
+                    position = i;
+                }
+            }
+            System.out.println("\nMaximum integer is : "+max);
+            System.out.println("Maximum integer position is : "+position+" in array.");
+        }
+
+        //Printing the array.
+        public static void printArray(Float[] array) {
+            System.out.println("Array is : ");
+            for (int i = 0; i < array.length; i++)
+            {
+                System.out.print(array[i]+"  ");
+            }
+        }
+
+        //Adding values to array.
+        public static Float[] addToArray() {
+            System.out.println("Enter 3 values");
+            Scanner scan = new Scanner(System.in);
+            float a = scan.nextFloat();
+            float b = scan.nextFloat();
+            float c = scan.nextFloat();
+            scan.close();
+            return new Float[]{a,b,c};
+        }
+    }
